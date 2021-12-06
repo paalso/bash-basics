a3838c4f450a7e19ff05ff5108aabae2e2ae690d
paalso <40a.pvl@gmail.com>
2021-12-06 21:23:55 +0200

    Implemented script filecopy.sh (for controlled copy a file to another one)

--- bash_samples/filecopy.sh
+++ bash_samples/filecopy.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/bash
 
-# Check if file1 exists and is file (not a directory)
-# and copy to file2
+# Checks if file1 exists and is a file (not a directory)
+# and copies to file2
 
+if [[ $# -ne 2 ]]
+then
+  echo "Usage: file-copy-from file-copy-to"
+  exit 1
+fi
+
+file1=$1; file2=$2
+
+if [[ ! -e $file1 ]]
+then
+  echo "Can't copy: path $file1 doesn't exist"
+  exit 1
+fi
+
+if [[ -d $file1 ]]
+then
+  echo "Can't copy: path $file1 is a directory"
+  exit 1
+fi
+
+if [[ -e $file2 ]]
+then
+  echo "Can't copy: path $file2 already exists"
+  exit 1
+fi
+
+cp $file1 $file2
+echo "$file1 succesfully copied to $file2"
+exit 0
