7033a095d383d1fa69d275d764878c63607caeb7
paalso <40a.pvl@gmail.com>
2021-12-07 12:25:05 +0200

    Implemented script create.sh (for controlled creating new file or directory)

--- bash_samples/arguments.sh
+++ bash_samples/arguments.sh
@@ -3,4 +3,3 @@
 echo "The first two arguments of the script $0 are:"
 echo "\$1=$1 \$2=$2"
 echo "Total number of arguments is $#"
-
---
+++ bash_samples/create.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+# Generate file or dir depending on the parameter
+
+if [[ $# -ne 2 ]]
+then
+  echo "Usage: type(f for file, d for dir) pathname"
+  exit 1
+fi
+
+type=$1; pathname=$2
+
+if [[ -e $pathname ]]
+then
+  echo "Cannot create $pathname: already exists"
+  exit 1
+fi
+
+if [[ $type == "f" ]]
+then
+  touch $pathname
+  echo "File $pathname succesfully created"
+  exit 0
+elif [[ $type == "d" ]]
+then
+  mkdir $pathname
+  echo "Directory $pathname succesfully created"
+  exit 0
+else
+  echo "Cannot create $pathname: wrong parameter $type"
+  exit 1
+fi
