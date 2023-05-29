79b72922248750074c2f763ee5594485bbc08a4a
Pavel Soroka <40a.pvl@gmail.com>
2023-05-29 11:38:28 +0300

    Update inithtml.sh

--- htmlinit/inithtml.sh
+++ htmlinit/inithtml.sh
@@ -1,42 +1,55 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# Закинуть файл в удобный каталог, указанный в PATH,
-# например ~/local/bin чтобы запускать из произвольной директории
-# Можно создать ссылки на этот файлы с другими похожими или
-# подходящими именами (напр. htmlinit), если пользователь
-# не запоминает
+# Function to create the regular project structure
+create_default_structure() {
+    mkdir -p "$1/css" "$1/img"
+    echo "* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}" > "$1/css/styles.css"
 
-if [[ $# != 1 ]]
-then
-  echo "Usage: $0 ProjectName"
-  exit 1
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>Document</title>
+</head>
+<body>
+</body>
+</html>' > "$1/index.html"
+    echo "Project $1 structure created"
+}
+
+# Function to create the Hexlet project structure
+create_hexlet_structure() {
+    mkdir -p "$1/styles" "$1/img"
+    echo "body {
+  margin: 0;
+}" > "$1/styles/app.css"
+
+    echo '<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/app.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <main>
+    </main>
+  </body>
+</html>' > "$1/index.html"
+    echo "Hexlet project $1 structure created"
+}
+
+# Check if -h or --hexlet argument is present
+if [[ $1 = "-h" ]] || [[ $1 = "--hexlet" ]]; then
+    create_hexlet_structure "$2"
+else
+    create_default_structure "$1"
 fi
-
-projectName=$1
-
-echo "Creating project directory $projectName"
-mkdir -p $projectName/css $projectName/img
-
-cd $projectName
-touch index.html
-echo '<!DOCTYPE html>' >> index.html
-echo '<html lang="en">' >> index.html
-echo '<head>' >> index.html
-echo '  <meta charset="UTF-8">' >> index.html
-echo '  <meta http-equiv="X-UA-Compatible" content="IE=edge">' >> index.html
-echo '  <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> index.html
-echo '  <link rel="stylesheet" href="css/styles.css">' >> index.html
-echo '  <title>Document</title>' >> index.html
-echo '</head>' >> index.html
-echo '<body>' >> index.html
-echo '    ' >> index.html
-echo '</body>' >> index.html
-echo '</html>' >> index.html
-
-cd css
-touch styles.css
-echo '* {' >> styles.css
-echo '  margin: 0;' >> styles.css
-echo '  padding: 0;' >> styles.css
-echo '  box-sizing: border-box;' >> styles.css
-echo '}' >> styles.css
